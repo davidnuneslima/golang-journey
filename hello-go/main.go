b7fac0b1104989f8b64613d969bd757a1e89148c
David Nunes <davidnuneslima@gmail.com>
2024-11-19 15:00:20 -0300

    feat: formatação dos diretórios hello-go e operators

--- hello-go/main.go
+++ hello-go/main.go
@@ -6,6 +6,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
-	nome,sobrenome := fmt.Println("David Nunes")
-	fmt.Println(nome,sobrenome)
+	nome, sobrenome := fmt.Println("David Nunes")
+	fmt.Println(nome, sobrenome)
 }
